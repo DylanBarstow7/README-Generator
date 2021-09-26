9ac59502fc96bc7b9adefa1e28d406fe82808893
Dylan Barstow <dcbarstow7@gmail.com>
2021-09-26 17:18:54 -0500

    Render license badge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license ==="Apache 2.0") {
+    return `![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
+  } 
+  else if (license === "GNU GPL v2") {
+    return `![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)`;
+  }
+  else if (license === "MIT") {
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  }
+  else (license === "No License") 
+    return ( ``)
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
