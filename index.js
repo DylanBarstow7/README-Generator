4e7cd961306e083ff97157c8c50c74b167ce17d4
Dylan Barstow <dcbarstow7@gmail.com>
2021-09-26 17:17:06 -0500

    Input questions

--- index.js
+++ index.js
@@ -4,7 +4,54 @@ const generateMarkdown = require("./utils/generate")
 const fs = require("fs")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {   
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {   
+        type: 'input',
+        name: 'title',
+        message: 'Enter a description of your project.'
+    },
+    {   
+        type: 'input',
+        name: 'title',
+        message: 'How does this project get used?'
+    },
+    {   
+        type: 'input',
+        name: 'title',
+        message: 'What are the installation instructions for your project?'
+    },
+    {   
+        type: 'input',
+        name: 'title',
+        message: 'How can users contribute to this application?'
+    },
+    {   
+        type: 'input',
+        name: 'title',
+        message: 'How do you test your application?'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license should be used?',
+        choices: ["Apache 2.0", "GNU GPL v2","MIT", "No License"]
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
