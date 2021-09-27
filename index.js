de9b8c4190400490522691f698683840fc726daa
Dylan Barstow <dcbarstow7@gmail.com>
2021-09-26 19:26:06 -0500

    corrected error causing undefined results

--- index.js
+++ index.js
@@ -7,49 +7,49 @@ const path = require("path")
 // TODO: Create an array of questions for user input
 const questions = [
     {   
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
     },
     {   
-        type: 'input',
-        name: 'title',
-        message: 'Enter a description of your project.'
+        type: "input",
+        name: "description",
+        message: "Enter a description of your project."
     },
     {   
-        type: 'input',
-        name: 'title',
-        message: 'How does this project get used?'
+        type: "input",
+        name: "usage",
+        message: "How does this project get used?"
     },
     {   
-        type: 'input',
-        name: 'title',
-        message: 'What are the installation instructions for your project?'
+        type: "input",
+        name: "installation",
+        message: "What are the installation instructions for your project?"
     },
     {   
-        type: 'input',
-        name: 'title',
-        message: 'How can users contribute to this application?'
+        type: "input",
+        name: "contribution",
+        message: "How can users contribute to this application?"
     },
     {   
-        type: 'input',
-        name: 'title',
-        message: 'How do you test your application?'
+        type: "input",
+        name: "test",
+        message: "How do you test your application?"
     },
     {
-        type: 'input',
-        name: 'username',
-        message: 'What is your GitHub username?',
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?",
     },
     {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email?',
+        type: "input",
+        name: "email",
+        message: "What is your email?",
     },
     {
-        type: 'list',
-        name: 'license',
-        message: 'Which license should be used?',
+        type: "list",
+        name: "license",
+        message: "Which license should be used?",
         choices: ["Apache 2.0", "GNU GPL v2","MIT", "No License"]
     },
 ];
@@ -68,7 +68,7 @@ function init() {
         let inputString = generateMarkdown(response)
         console.log(inputString)
 
-        writeToFile('README.md', inputString)
+        writeToFile("README.md", inputString)
     })
 }
 
