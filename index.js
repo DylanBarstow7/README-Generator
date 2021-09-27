49a01fddd470ee98c73c892b9f58050adf31a563
Dylan Barstow <dcbarstow7@gmail.com>
2021-09-26 19:12:45 -0500

    created path package

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require("inquirer")
 const generateMarkdown = require("./Assets/utils/js/generateMarkdown.js")
 const fs = require("fs")
+const path = require("path")
 
 // TODO: Create an array of questions for user input
 const questions = [
